40ddcf7c5ec03782e4364ed19a71ecaef6007430
saswatisamal <saswatisamal02@gmail.com>
2022-08-23 19:16:55 +0530

    Adding logs

--- src/main/java/com/in30minutes/accounts/AccountsApplication.java
+++ src/main/java/com/in30minutes/accounts/AccountsApplication.java
@@ -1,5 +1,7 @@
 package com.in30minutes.accounts;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -9,8 +11,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(value= {"com.in30minutes.repository"})
 @EntityScan({"com.in30minutes.model"})
 public class AccountsApplication {
+	static Logger logger=LoggerFactory.getLogger(AccountsApplication.class);
+	
 
 	public static void main(String[] args) {
+		logger.info("Accounts Microservice:::");
 		SpringApplication.run(AccountsApplication.class, args);
 	}
 
